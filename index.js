1d2447b4d7e4c30879496893244a42eff485d640
piperpei <piperpei@yahoo.com>
2025-07-25 13:45:54 -0700

    I FORGOT TO COMMIT AGAIN

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 //Write your code here
 
+// Attendee Object 
 
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
+
+// Function Logging Attendee Name 
+
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
+
+// Function Log Ticket Price 
+
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
+
+// Update Ticket Type 
+
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType; 
+}
+
+// Update Ticket Price 
+
+function updateTicketPrice(attendee, newTicketPrice) {
+  attendee.ticketPrice = newTicketPrice;
+}
+
+// Function Remove Event Propery 
+
+function removeEventProperty(attendee) {
+  delete attendee.event; 
+}
+
+// Check In Function 
+
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true; 
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
